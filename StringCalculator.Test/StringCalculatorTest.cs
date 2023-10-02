80c1bd0d2f020a891452923a367655781f4184a3
dinog <dinog@LAPTOP-261I51LB>
2023-10-02 11:49:31 -0600

    Agregar archivos de proyecto.

---
+++ StringCalculator.Test/StringCalculatorTest.cs
@@ -0,0 +1,75 @@
+namespace StringCalculator.Test
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+         public void RetornaCeroSiReciveCadenaVacia()
+        {
+            //Arrange
+            StringCalculatorService service= new StringCalculatorService();
+
+            //Act
+            int resultado = service.Add("");
+
+            //Assert
+           Assert.AreEqual(0, resultado);
+        }
+
+
+        [TestMethod]
+
+        [DataRow("5", 5)]
+        [DataRow("10000", 10000)]
+        [DataRow("89234", 89234)]
+        [DataRow("8998", 8998)]
+        public void RetornaCeroSiReciveNumero(String number, int ResultadoCorrecto)
+        {
+            //Arrange
+            StringCalculatorService service = new StringCalculatorService();
+
+            //Act
+            int resultado = service.Add(number);
+
+            //Assert
+            Assert.AreEqual(ResultadoCorrecto, resultado);
+        }
+
+
+        [TestMethod]
+        public void RetornaCeroSiReciveDosNumeros()
+        {
+            //Arrange
+            StringCalculatorService service = new StringCalculatorService();
+
+            //Act
+            int resultado = service.Add("5,10");
+
+            //Assert
+            Assert.AreEqual(15, resultado);
+        }
+
+        [TestMethod]
+        [DataRow("5,5,3", 13)]
+        [DataRow("5,5\n3,1\n2", 16)]
+        [DataRow("1,1,1,1,1", 5)]
+        [DataRow("5,10,10,5", 30)]
+        [DataRow("//;\n5;10;10;5", 30)]
+        [DataRow("//%\n5%10%10%5", 30)]
+        [DataRow("//;\n5;10;10;5;-10;-4", 30)]
+
+
+
+        public void RetornaCeroSiReciveMuchosNumeros(string numeros, int resultadoCorrecto )
+        {
+            //Arrange
+            StringCalculatorService service = new StringCalculatorService();
+
+            //Act
+            int resultado = service.Add(numeros);
+
+            //Assert
+            Assert.AreEqual(resultadoCorrecto, resultado);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculatorService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace StringCalculator
+{
+    public class StringCalculatorService
+    {
+        public int Add(string numbers) 
+        {
+            char[] separador = {'\n',  ','};
+            int suma = 0;
+            
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            
+            if (numbers.StartsWith("//"))
+            {
+                char [] arrayNumbers = numbers.ToCharArray();
+                char[] limitadores = { arrayNumbers[2], '\n', '/' };
+
+                var parts2 = numbers.Split(limitadores, StringSplitOptions.RemoveEmptyEntries);
+
+                List<int> list = new List<int>();
+                for (int i = 0; i < parts2.Length; i++)
+                {
+                    if (int.Parse(parts2[i]) < 0)
+                    {
+                        list.Add(int.Parse(parts2[i]));
+                    }
+                    else
+                    {
+                        suma = suma + int.Parse(parts2[i]);
+                    }
+                }
+                if (list != null)
+                {
+                    Console.WriteLine("Los numeros negativos no son permitidos: "+ list);
+                }
+
+                return suma;
+            }
+            
+            var parts=numbers.Split(separador, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < parts.Length; i++)
+            {
+                suma = suma + int.Parse(parts[i]);
+            }
+
+            return suma;
+        }
+    }
+}
